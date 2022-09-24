79ad659c1ffa574d9c36902cfe4efe89a5d7fe8c
daieun <jdaieun@gmail.com>
2022-09-24 11:17:24 +0900

    Update talker.launch.py

--- launch/talker.launch.py
+++ launch/talker.launch.py
@@ -2,7 +2,7 @@ from launch import launch_description
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription(
+    return launchdescription(
         Node(
             package='demo_nodes_cpp',
             executable='talker'
